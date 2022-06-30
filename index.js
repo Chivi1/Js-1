b9e893572f13530bc6077f58197e275ad9d7a682
chivi <paulofr016@gmail.com>
2022-06-30 14:57:33 -0300

    tp2

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let x = 1;
-let linea
-while(x <= 6){
-    console.log(x)
-    x++;
-} 
-console.log('fin de while')
+let nombre = prompt("ingresa tu nombre");
+let apellido = prompt("ingresa tu apellido");
+let nick = prompt("ingresa tu nickname para personalizar tu producto");
+let email = prompt("ingresa tu email");
+let direccion = prompt("Ingresa la dirección adonde quieras enviar el pedido");
+let ubicacion = prompt("ingresa la provincia a la que perteneces (Solo Cuyo)").toLowerCase();
+
+console.log(nombre, ubicacion, email);
+
+if ((ubicacion == "mendoza") || (ubicacion == "san juan") || (ubicacion == "san luis")){
+    alert("¡El envío se realizara a tu provincia cuando el producto este listo!");
+    alert("tu pedido se enviará a: " + direccion);
+} else { 
+        alert("¡Aún no hacemos envios a tu provincia!");
+    }
+
+
+
+
+
